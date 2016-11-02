384dbc43179c804f1b65fe5b7933263cb5e5c32f
April Mulvey <april.mulvey@gmail.com>
2016-11-02 03:52:52 +0000

    have chatty app accessing event target and logging what was typed in the input field

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,21 +1,22 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+
   handleKeyUp(event) {
     if (event.key === 'Enter') {
-      console.log(event.target.value);
+      this.props.addMessage(event.target.value);
     }
-   
   }
 
   render() {
-    this.props.addMessage("Hello");
+    // WHY DO WE HAVE TO BIND THIS TO HANDLEKEYUP WHYYYYYYY ( probably because ES6 classes )
     return (
       <footer>
         <input id="username" type="text" placeholder={this.props.currentUser.name} />
-        <input id="new-message" type="text" placeholder="Type a message and hit ENTER" onKeyUp={this.handleKeyUp} />
+        <input id="new-message" type="text" placeholder="Type a message and hit ENTER" onKeyUp={(event) => this.handleKeyUp(event)} />
       </footer>
     );
   }
+
 }
 export default ChatBar;
