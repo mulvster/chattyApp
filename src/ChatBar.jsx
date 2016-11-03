a7a4521de8489aab500c30b81f31b13af6f8d901
April Mulvey <april.mulvey@gmail.com>
2016-11-03 03:42:23 +0000

    rendering on both sides and user can change name

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,25 +2,35 @@ import React, {Component} from 'react';
 
 
 class ChatBar extends Component {
-
-  handleKeyUp(event) {
+  constructor(props) {
+    super(props);
+    this.state = {message: " ", user: " "};
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleMessageChange = this.handleMessageChange.bind(this);
+  }
+  handleMessageChange(event) {
     if (event.key === 'Enter') {
+      this.props.addMessage({message: this.state.message, user: this.state.user});
+    }else {
+      this.setState({message: event.target.value});
       // sends message to parent
-      this.props.addMessage(event.target.value);
+      // this.props.addMessage(event.target.value);
 
       // reset the input field to blank
         // insert awesome code here
     }
   }
-
+handleNameChange(event) {
+  this.setState({user: event.target.value});
+}
   render() {
     // WHY DO WE HAVE TO BIND THIS TO HANDLEKEYUP WHYYYYYYY ( probably because ES6 classes )
   //React.createClass automatically binds "this" for you. WHY?! HOW?!
     //why does render have it... but handleKeyUp does not? "this" was null before the bind.
     return (
       <footer>
-        <input id="username" type="text" placeholder={this.props.currentUser.name} />
-        <input id="new-message" type="text" placeholder="Type a message and hit ENTER" onKeyUp={this.handleKeyUp.bind(this)} />
+        <input id="username" type="text" placeholder={this.props.currentUser.name} onKeyUp={this.handleNameChange}  />
+        <input id="new-message" type="text" placeholder="Type a message and hit ENTER" onKeyUp={this.handleMessageChange} />
       </footer>
     );
   }
